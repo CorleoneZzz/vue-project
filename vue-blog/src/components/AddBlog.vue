c73fed9ba6376b1b837af173fb4f1da8ede0f2d2
CorleoneG <1410131029@qq.com>
2019-06-18 17:27:16 +0800

    添加博客页面

--- vue-blog/src/components/AddBlog.vue
+++ vue-blog/src/components/AddBlog.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="add-blog">
     <h2>添加博客</h2>
-    <form>
+    <form v-if="!summited">
       <label>博客标题</label>
       <input v-model="title" type="text"/>
       <label >博客内容</label>
@@ -20,12 +20,15 @@
       <select v-model="author">
         <option v-for="author in authors"  :key=author>{{author}}</option>
       </select>
-     <!--prevent阻止刷新页面 -->
-      <button v-on:click.prevent="post"></button>
     </form>
+    <!--prevent阻止刷新页面 -->
+    <button v-on:click.prevent="post">添加博客</button>
 
     <hr>
 
+    <div v-if="summited">
+      <h3>您的博客已经添加成功</h3>
+    </div>
     <div id="preview">
       <h3>博客总览</h3>
       <p>博客标题：{{title}}</p>
@@ -55,12 +58,63 @@
           content:'',
           categories: [],
           author:'',
-          authors:["Amani","Berry","Cherry"]
+        authors: ["Amani", "Berry", "Cherry"],
+        summited: false
         }
-    }
+    },
+    methods:
+      {
+        post() {
+          this.$http.post("http://jsonplaceholder.typicode.com/posts"
+            , {
+              title: this.title,
+              body: this.content,
+              userId: 1
+            }).then((data) => {
+            console.log(data);
+            this.summited = true;
+          })
+        }
+      }
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .add-blog {
+    margin: 20px auto;
+    max-width: 600px;
+    padding: 20px;
 
+    textarea, input, select {
+      width: 100%;
+    }
+
+    label {
+      display: block;
+    }
+
+    #checkbox {
+      label, input {
+        display: inline-block;
+      }
+
+      input {
+        width: 10%;
+      }
+    }
+
+    button {
+      color: #ffffff;
+      border-radius: 4px;
+      background: crimson;
+      padding: 10px;
+      margin-top: 10px;
+    }
+
+    #preview {
+      border: 1px solid gainsboro;
+      padding: 20px;
+      margin-top: 30px;
+    }
+  }
 </style>
