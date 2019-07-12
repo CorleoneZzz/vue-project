fb04ae73bb40755fe47b00684a1f5ce0abc87029
CorleoneG <1410131029@qq.com>
2019-07-12 10:17:01 +0800

    update

--- shopping-mall/src/App.vue
+++ shopping-mall/src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import GoodList from "./views/GoodList";
 export default {
-  name: 'App'
+  name: 'App',
+  components: {GoodList}
 }
 </script>
 
