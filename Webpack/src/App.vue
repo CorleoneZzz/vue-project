8a3b8ae0269e2c9bdbe6f981529161707c151fbe
CorleoneG <1410131029@qq.com>
2019-07-14 16:34:50 +0800

    change name as webpack

---
+++ Webpack/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- <div id="app">-->
+    <!--    <h2 class="color-red">{{ text }}</h2>-->
+    <!--  </div>-->
+    <h2 class="color-red">{{ text }}</h2>
+</template>
+
+<script>
+    export default {
+        name: "App",
+        data() {
+            return {
+                text: "Hello, Vue!"
+            };
+        }
+    }
+</script>
+
+<style scoped>
+    .color-red {
+        color: red;
+    }
+</style>
--- Webpack/src/main.js
+++ Webpack/src/main.js
@@ -3,11 +3,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: { App },
   template: '<App/>'
-})
+});
