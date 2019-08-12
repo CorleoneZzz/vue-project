0ee0c8f4d4194ae93f32962eff030387ff844950
CorleoneG <1410131029@qq.com>
2019-08-12 17:11:46 +0800

    组件化任务功能

--- ToDoList/js/storage.js
+++ ToDoList/js/storage.js
@@ -1,18 +1,17 @@
 ;(function () {
-    window.lo =
-        {
-            get: get,
-            set: set
-        };
+    window.ms = {
+        set: set,
+        get: get,
+    };
 
     function set(key, val) {
         localStorage.setItem(key, JSON.stringify(val));
     }
 
     function get(key) {
-        let json = localStorage.getItem(key);
+        var json = localStorage.getItem(key);
         if (json) {
-            return JSON.parse(json)
+            return JSON.parse(json);
         }
     }
 })();
